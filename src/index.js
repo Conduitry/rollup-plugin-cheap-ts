a02bc3ac4c2ae5dc01e08278f395ff94f8e0c87e
Conduitry <git@chor.date>
2018-11-10 17:39:08 -0500

    add some checking of user-supplied tsconfig.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
-
 export default () => {
-	let transform;
+	const fs = require('fs');
+	const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json').toString());
+	if (!tsconfig.include) {
+		throw new Error('tsconfig.json must specify "include" configuration');
+	}
+	if (!tsconfig.compilerOptions) {
+		throw new Error('tsconfig.json must specify "compilerOptions" configuration');
+	}
+	const target = tsconfig.compilerOptions.target && tsconfig.compilerOptions.target.toLowerCase();
+	if (!target || target === 'es3' || target === 'es5') {
+		throw new Error('tsconfig.json must specify "compilerOptions.target" of at least ES6');
+	}
 
+	let transform;
 	if (process.env.ROLLUP_WATCH === 'true') {
 		const { transpileModule } = require('typescript');
-		const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json').toString());
 		transform = (code, id) => {
 			if (id.endsWith('.ts')) {
 				const { outputText, sourceMapText } = transpileModule(code, tsconfig);
