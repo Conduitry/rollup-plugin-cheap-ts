8c6b05166e7d4dbf873152e9096b67912bd7d55b
Conduitry <git@chor.date>
2018-11-11 17:43:14 -0500

    improve tsconfig.json validation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 export default () => {
 	const fs = require('fs');
 	const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json').toString());
-	if (!tsconfig.include) {
-		throw new Error('tsconfig.json must specify "include" configuration');
+	const { compilerOptions } = tsconfig;
+	if (!compilerOptions || !compilerOptions.target || compilerOptions.target.toLowerCase() === 'es3' || compilerOptions.target.toLowerCase() === 'es5') {
+		throw new Error('tsconfig.json must specify "compilerOptions"."target" of at least "ES6"');
 	}
-	if (!tsconfig.compilerOptions) {
-		throw new Error('tsconfig.json must specify "compilerOptions" configuration');
-	}
-	const target = tsconfig.compilerOptions.target && tsconfig.compilerOptions.target.toLowerCase();
-	if (!target || target === 'es3' || target === 'es5') {
-		throw new Error('tsconfig.json must specify "compilerOptions.target" of at least ES6');
-	}
+	['inlineSourceMap', 'module', 'noEmit', 'outDir', 'outFile'].forEach(key => {
+		if (compilerOptions[key]) {
+			throw new Error(`tsconfig.json must not specify "compilerOptions"."${key}"`);
+		}
+	});
 
 	let transform;
 	if (process.env.ROLLUP_WATCH === 'true') {
