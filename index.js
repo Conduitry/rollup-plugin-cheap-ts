b207cc83e48cf22dfe225f51309527d2286ee52e
Conduitry <git@chor.date>
2018-11-12 08:19:18 -0500

    allow multiple simultaneous builds on same set of input files

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
+let tsconfig;
+const files = new Set();
+
 module.exports = () => {
-	const fs = require('fs');
-	const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json').toString());
-	const { compilerOptions } = tsconfig;
-	if (!compilerOptions || !compilerOptions.target || compilerOptions.target.toLowerCase() === 'es3' || compilerOptions.target.toLowerCase() === 'es5') {
-		throw new Error('tsconfig.json must specify "compilerOptions"."target" of at least "ES6"');
-	}
-	['inlineSourceMap', 'module', 'noEmit', 'outDir', 'outFile'].forEach(key => {
-		if (compilerOptions[key]) {
-			throw new Error(`tsconfig.json must not specify "compilerOptions"."${key}"`);
+	if (!tsconfig) {
+		tsconfig = JSON.parse(fs.readFileSync('tsconfig.json').toString());
+		const { compilerOptions } = tsconfig;
+		if (!compilerOptions || !compilerOptions.target || compilerOptions.target.toLowerCase() === 'es3' || compilerOptions.target.toLowerCase() === 'es5') {
+			throw new Error('tsconfig.json must specify "compilerOptions"."target" of at least "ES6"');
 		}
-	});
+		['inlineSourceMap', 'module', 'noEmit', 'outDir', 'outFile'].forEach(key => {
+			if (compilerOptions[key]) {
+				throw new Error(`tsconfig.json must not specify "compilerOptions"."${key}"`);
+			}
+		});
+		if (process.env.ROLLUP_WATCH !== 'true') {
+			require('child_process').execSync('tsc', { stdio: ['ignore', 'inherit', 'inherit'] });
+			const unlinkQuiet = file => {
+				try {
+					fs.unlinkSync(file);
+				} catch (e) {}
+			};
+			process.on('exit', () => files.forEach(unlinkQuiet));
+		}
+	}
 
 	let transform;
 	if (process.env.ROLLUP_WATCH === 'true') {
@@ -24,20 +40,18 @@ module.exports = () => {
 		};
 	} else {
 		const readFileAsync = require('util').promisify(fs.readFile);
-		require('child_process').execSync('tsc', { stdio: ['ignore', 'inherit', 'inherit'] });
 		transform = async (code, id) => {
 			if (id.endsWith('.ts')) {
 				const jsPromise = readFileAsync(id.slice(0, -2) + 'js').then(data => data.toString());
 				const mapPromise = readFileAsync(id.slice(0, -2) + 'js.map').then(data => JSON.parse(data.toString()), () => null);
 				const [code, map] = await Promise.all([jsPromise, mapPromise]);
-				fs.unlink(id.slice(0, -2) + 'js', () => null);
-				fs.unlink(id.slice(0, -2) + 'js.map', () => null);
+				files.add(id.slice(0, -2) + 'js');
+				files.add(id.slice(0, -2) + 'js.map');
 				return { code, map };
 			}
 		};
 	}
 
-	const path = require('path');
 	return {
 		name: 'cheap-ts',
 		resolveId(importee, importer) {
