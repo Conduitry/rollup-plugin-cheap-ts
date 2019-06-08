e8eaa775e6182a6046b7730729d6e074f7c59cd7
Conduitry <git@chor.date>
2019-06-08 09:05:02 -0400

    style

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 'use strict';
 
-const isWatch = process.env.ROLLUP_WATCH === 'true';
+const is_watch = process.env.ROLLUP_WATCH === 'true';
 
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
-const typescript = isWatch && require('typescript');
+const typescript = is_watch && require('typescript');
 
 let tsconfig;
 const files = new Set();
-const readFileAsync = (...args) => new Promise((res, rej) => fs.readFile(...args, (err, data) => (err ? rej(err) : res(data))));
+const readFile_async = (...args) => new Promise((res, rej) => fs.readFile(...args, (err, data) => err ? rej(err) : res(data)));
 function resolveId(importee, importer) {
 	if ((!importer || importer.endsWith('.ts')) && !path.extname(importee)) {
 		return this.resolveId(importee + '.ts', importer);
 	}
 }
-const transform = isWatch
+const transform = is_watch
 	? (code, id) => {
-			if (id.endsWith('.ts')) {
-				const { outputText, sourceMapText } = typescript.transpileModule(code, tsconfig);
-				return { code: outputText, map: JSON.parse(sourceMapText) };
-			}
-	  }
+		if (id.endsWith('.ts')) {
+			const { outputText, sourceMapText } = typescript.transpileModule(code, tsconfig);
+			return { code: outputText, map: JSON.parse(sourceMapText) };
+		}
+	}
 	: (code, id) => {
-			if (id.endsWith('.ts')) {
-				files.add(id.slice(0, -2) + 'js');
-				files.add(id.slice(0, -2) + 'js.map');
-				return Promise.all([readFileAsync(id.slice(0, -2) + 'js').then(data => data.toString()), readFileAsync(id.slice(0, -2) + 'js.map').then(data => JSON.parse(data.toString()), () => null)]).then(([code, map]) => ({ code, map }));
-			}
-	  };
-const unlinkQuiet = file => {
+		if (id.endsWith('.ts')) {
+			files.add(id.slice(0, -2) + 'js');
+			files.add(id.slice(0, -2) + 'js.map');
+			return Promise.all([readFile_async(id.slice(0, -2) + 'js').then(data => data.toString()), readFile_async(id.slice(0, -2) + 'js.map').then(data => JSON.parse(data.toString()), () => null)]).then(([code, map]) => ({ code, map }));
+		}
+	};
+const unlink_quiet = file => {
 	try {
 		fs.unlinkSync(file);
 	} catch (e) {}
@@ -47,9 +47,9 @@ module.exports = () => {
 				throw new Error(`tsconfig.json must not specify "compilerOptions"."${key}"`);
 			}
 		}
-		if (!isWatch) {
+		if (!is_watch) {
 			child_process.execSync('tsc', { stdio: ['ignore', 'inherit', 'inherit'] });
-			process.on('exit', () => files.forEach(unlinkQuiet));
+			process.on('exit', () => files.forEach(unlink_quiet));
 		}
 	}
 
